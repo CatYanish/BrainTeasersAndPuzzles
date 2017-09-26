914c821426f973f44a0db76a429847c27d5e3c3e
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-25 23:24:08 -0500

    completed consecutive number challenge

--- client.js
+++ client.js
@@ -1,23 +1,25 @@
 console.log('js sourced');
 
-var input = [1,1,0,1,1,1];
+var input = [1,0,1,1,0,1]
 
 var consecutiveNum = 0;
+var maxConsec = 0;
 
-// findMaxConsecutiveOnes(input);
 
 var findMaxConsecutiveOnes = function(nums) {
     for (var i = 0; i < nums.length; i++) {
       if (nums[i] === 1) {
         consecutiveNum += 1;
-        console.log(consecutiveNum);
+        if (consecutiveNum > maxConsec) {
+              maxConsec = consecutiveNum;
+        }
       } else {
         consecutiveNum = 0;
       }
     }
-    return consecutiveNum
+    return maxConsec
 };
 
 findMaxConsecutiveOnes(input);
 
-console.log("consecutiveNum", consecutiveNum);
+console.log("consecutiveNum", maxConsec);
