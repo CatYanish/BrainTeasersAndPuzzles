d17b64cd4764275ac20633f0d7f07d166cdcd52b
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-27 13:25:20 -0500

    solved return middle of string

--- client.js
+++ client.js
@@ -1,25 +1,42 @@
 console.log('js sourced');
 
 var input = [1,0,1,1,0,1]
-
-var consecutiveNum = 0;
-var maxConsec = 0;
-
-
+var currentStreak = 0;
+var maxAllTimeStreak = 0;
+// this function will find the maximum number of consecutive
+//ones in a binary array. It compares the current streak to the max
+//streak ever. If the streak is broken,
+//the counter resets but the maxStreak record donest reset
 var findMaxConsecutiveOnes = function(nums) {
-    for (var i = 0; i < nums.length; i++) {
-      if (nums[i] === 1) {
-        consecutiveNum += 1;
-        if (consecutiveNum > maxConsec) {
-              maxConsec = consecutiveNum;
-        }
-      } else {
-        consecutiveNum = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      currentStreak += 1;
+      if (currentStreak > maxAllTimeStreak) {
+        maxAllTimeStreak = currentStreak;
       }
+    } else {
+      currentStreak = 0;
     }
-    return maxConsec
+  }
+  return maxAllTimeStreak
 };
+findmaxAllTimeStreakutiveOnes(input);
+console.log("currentStreak", maxAllTimeStreak);
 
-findMaxConsecutiveOnes(input);
 
-console.log("consecutiveNum", maxConsec);
+
+//this function takes in a string and returns the middle character
+//or middle two characters
+function getMiddle(s){
+  if(s.length % 2 !== 0) {
+    console.log(s.charAt(s.length/2));
+    return s.charAt(s.length/2);
+  } else {
+    var mean = s.length/2
+    var mid1 = s.charAt(Math.floor(mean));
+    var mid2 = s.charAt(Math.ceil(mean));
+    console.log(mid1+mid2);
+    return mid1+mid2;
+  }
+}
+getMiddle('apple');
